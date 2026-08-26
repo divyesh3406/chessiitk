0614529674f528fef34a3ac45908df32b078279b
divyesh3406 <divyeshb25@iitk.ac.in>
2026-08-26 19:33:56 +0530

    changes

--- react-app/src/pages/Blogs.jsx
+++ react-app/src/pages/Blogs.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+ import { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { API_BASE_URL } from '../config';
